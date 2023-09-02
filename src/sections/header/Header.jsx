d945047c362835c2c51c85249984054d3b333450
akbaralinabiev <akbaralinabiev14@gmail.com>
2023-09-02 19:31:28 +0200

    nav menu added

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import "./header.css";
 import "./style.scss";
 import "./custom.js";
 import { video } from "./import";
 
+const Menu = () => {
+  return (
+    <>
+      <div className="nav-links-mobile">
+            <ul>
+              <li>
+                <a href="#home">home</a>
+                <a href="#explore">explore</a>
+                <a href="#contact">contact</a>
+              </li>
+            </ul>
+          </div>
+    </>
+  );
+};
 
 const Home = () => {
+  const [toggleMenu,setToggleMenu] = useState(false);
   return (
     <div id="home">
       <div className="wrapper">
@@ -21,6 +38,28 @@ const Home = () => {
                 <a href="#contact">contact</a>
               </li>
             </ul>
+          </div>
+          <div className="navbar-menu"> 
+            {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="gpt3__navbar-menu_container scale-up-center">
+            <div className="gpt3__navbar-menu_container-links">
+              <Menu />
+            </div>
+          </div>
+        )}
           </div>
         </div>
         
