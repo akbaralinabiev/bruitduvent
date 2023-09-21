d4538f6b7f9c8875d3d56fc9374ab68daf717edb
akbaralinabiev <akbaralinabiev14@gmail.com>
2023-09-21 20:35:57 +0200

    updating

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -8,8 +8,8 @@ const Contact = () => {
     <div className='contact-wrapper' id='contact'>
       <div className='layer'></div>
         <div className='links-above'>
-            <a className="social-media" href="https://www.instagram.com/bruitduven.t/">instagram</a>
-            <a className="social-media" href="https://t.me/Stohoven">telegram</a>
+            <a className="social-media" href="#">instagram</a>
+            <a className="social-media" href="#">telegram</a>
             <a className='email' href={`mailto:${emailAddress}`}>duvent.photo@gmail.com</a>
         </div>
         <h4 className='copyright'> &copy; 2024 BRUITDUVENT Portfolio. All rights reserved.</h4>
